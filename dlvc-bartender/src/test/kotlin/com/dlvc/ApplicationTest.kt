f2b208b53eb0013f0a1bbf6a3a5a827003c13d52
BorisP <Boris.Prochnau@mail.de>
2024-01-13 16:16:31 +0100

    test: cover thymeleaf templating

--- dlvc-bartender/src/test/kotlin/com/dlvc/ApplicationTest.kt
+++ dlvc-bartender/src/test/kotlin/com/dlvc/ApplicationTest.kt
@@ -1,10 +1,12 @@
 package com.dlvc
 
 import com.dlvc.plugins.*
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import org.junit.Assert
 import kotlin.test.*
 
 class ApplicationTest {
@@ -18,6 +20,21 @@ class ApplicationTest {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
         }
+    }
+
+    @org.junit.Test
+    fun test() = testApplication {
+        val response: String = client.get("/index").body()
+        Assert.assertEquals(
+            """
+<html>
+    <body>
+        <h1>Hello, John</h1>
+    </body>
+</html>
+        """.trimIndent(),
+            response
+        )
     }
 
 }
