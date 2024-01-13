511af8cdf860e890fadd577d7c6baa85574b52fe
BorisP <Boris.Prochnau@mail.de>
2024-01-13 13:44:33 +0100

    test: add basic order tests

--- dlvc-bartender/build.gradle.kts
+++ dlvc-bartender/build.gradle.kts
@@ -4,13 +4,15 @@ val logback_version: String by project
 
 val postgres_version: String by project
 val h2_version: String by project
+val kotest = "4.6.3"
+
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
-
 group = "com.dlvc"
+
 version = "0.0.1"
 
 application {
@@ -23,7 +25,6 @@ application {
 repositories {
     mavenCentral()
 }
-
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
@@ -35,4 +36,5 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotest")
 }
---
+++ dlvc-bartender/src/test/kotlin/com/dlvc/routes/OrderRoutesKtTest.kt
@@ -0,0 +1,31 @@
+package com.dlvc.routes
+
+import com.dlvc.plugins.configureRouting
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldNotHaveLength
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+
+
+class OrderRoutesKtTest {
+
+    @Test
+    fun orderRouting() {
+        testApplication {
+            application {
+                configureRouting()
+            }
+            client.get("/order").apply {
+                status shouldBe HttpStatusCode.OK
+                bodyAsText() shouldNotHaveLength 0
+            }
+        }
+    }
+
+    @Test
+    fun totalizeOrderRoute() {
+    }
+}
