3c5221a6bb0311ea7116ad50a3ab26b9dff9999a
BorisP <Boris.Prochnau@mail.de>
2024-01-13 13:16:15 +0100

    feat: add order with routes

--- dlvc-bartender/src/main/kotlin/com/dlvc/plugins/Routing.kt
+++ dlvc-bartender/src/main/kotlin/com/dlvc/plugins/Routing.kt
@@ -1,11 +1,15 @@
 package com.dlvc.plugins
 
 import com.dlvc.routes.customerRouting
+import com.dlvc.routes.orderRouting
+import com.dlvc.routes.totalizeOrderRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        orderRouting()
+        totalizeOrderRoute()
     }
 }
