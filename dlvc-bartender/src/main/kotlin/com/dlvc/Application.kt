5e5f9106c4e086b6342cc3651e9393243b2c5d8b
BorisP <Boris.Prochnau@mail.de>
2024-01-13 16:10:06 +0100

    feat: add thymeleaf example

--- dlvc-bartender/src/main/kotlin/com/dlvc/Application.kt
+++ dlvc-bartender/src/main/kotlin/com/dlvc/Application.kt
@@ -1,9 +1,14 @@
 package com.dlvc
 
+import com.dlvc.models.Customer
 import com.dlvc.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.thymeleaf.*
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -11,6 +16,20 @@ fun main() {
 }
 
 fun Application.module() {
+    install(Thymeleaf) {
+        setTemplateResolver(ClassLoaderTemplateResolver().apply {
+            prefix = "templates/"
+            suffix = ".html"
+            characterEncoding = "utf-8"
+        })
+    }
+    routing {
+        get("/index") {
+            val sampleUser = Customer("1", "John", "Doe", "John.Doe@mail.com")
+            call.respond(ThymeleafContent("index", mapOf("user" to sampleUser)))
+        }
+    }
+
     configureSerialization()
     configureDatabases()
     configureRouting()
