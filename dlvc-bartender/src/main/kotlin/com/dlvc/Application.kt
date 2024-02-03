50185f209d92037e1bae808f42634106b2b1f3ab
BorisP <Boris.Prochnau@mail.de>
2024-02-03 21:53:23 +0100

    feat: post initial item to dealer context

--- dlvc-bartender/src/main/kotlin/com/dlvc/Application.kt
+++ dlvc-bartender/src/main/kotlin/com/dlvc/Application.kt
@@ -5,6 +5,7 @@ import com.dlvc.examples.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
@@ -26,10 +27,23 @@ fun Application.module() {
 
     routing {
         get("/dealer") {
-            call.respond(ThymeleafContent("dealer", emptyMap()))
+            call.respond(ThymeleafContent("dealer", mapOf("item" to Item("Beer", 3.0, 0.5, "alcohol", "A drink"))))
         }
+        post("/add") {
+            val post = call.receiveText()
+            call.respondText("Posted: $post")
+        }
+
     }
 
     configureSerialization()
     configureExampleRoutes()
 }
+
+data class Item(
+    val name: String,
+    val price: Double,
+    val weight: Double?,
+    val tags: String,
+    val description: String?
+)
